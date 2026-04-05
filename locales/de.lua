96e8d282a8f3e9ec9bd8920c2c962934ddb87915
T-G0W <50kgkidtgowfckedyou@gmail.com>
2026-04-05 08:37:20 +0200

    Create de.lua

---
+++ locales/de.lua
@@ -0,0 +1,9 @@
+Locales = Locales or {}
+
+Locales['de'] = {
+    ['aduty_on'] = 'Du bist nun als %s angemeldet.',
+    ['aduty_off'] = 'Du hast den Dienst verlassen und deine alte Kleidung angezogen.',
+    ['no_permission'] = 'Keine Berechtigung für den Aduty Befehl.',
+    ['discord_not_found'] = 'Deine Discord ID konnte nicht gefunden werden.',
+    ['error_restoring'] = 'Es gab ein Problem beim Wiederherstellen deiner alten Kleidung.'
+}
