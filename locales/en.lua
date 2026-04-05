88ee068cea4578bb912e7c825ce079895e800ed7
T-G0W <50kgkidtgowfckedyou@gmail.com>
2026-04-05 08:37:35 +0200

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,9 @@
+Locales = Locales or {}
+
+Locales['en'] = {
+    ['aduty_on'] = 'You are now on duty as %s.',
+    ['aduty_off'] = 'You have gone off duty and put on your old clothes.',
+    ['no_permission'] = 'You do not have permission to use the aduty command.',
+    ['discord_not_found'] = 'Your Discord ID could not be found.',
+    ['error_restoring'] = 'There was a problem restoring your old clothes.'
+}
